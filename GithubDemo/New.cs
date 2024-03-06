e5287333716a0eb727ae07a1b18dfa8e5849286d
Manoj Pant <pantmanoj2005@gmail.com>
2024-03-06 17:53:38 +0530

    Add new class and new properties.

---
+++ GithubDemo/New.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubDemo
+{
+    public class New
+    {
+        public int id;
+        public string name;
+        public string url;
+    }
+}
